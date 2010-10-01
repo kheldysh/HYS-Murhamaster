e18db17f106a9ba711fc6b8ada1dee24f8654737
kheldysh <kheldysh@ff25d568-61ca-42c6-9216-fe35c4661081>
2010-10-01 00:33:39 +0000

    Now shows registration deadline without "klo"; Targets do not become visible on side panel before game starts

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -6,11 +6,16 @@ class Tournament < ActiveRecord::Base
   has_many :referees
   has_many :users, :through => :referees
   
-  def app_deadline_fin
+  def app_deadline_formatted
     if app_deadline
-      return app_deadline.strftime("%d.%m.%Y klo %H.%M")
+      return app_deadline.strftime("%d.%m.%Y, %H.%M")
     end
+  end
+  
+  def is_running?
+    start_date <= Date.today and Date.today <= finish_date
   end
     
   named_scope :registration_open, :conditions => ["app_deadline > ?", Time.now]
+  
 end

d601b5426c2b0d066d2912dbb7ce2c489169ed9b
kheldysh <kheldysh@ff25d568-61ca-42c6-9216-fe35c4661081>
2010-10-01 01:22:20 +0000

    Refereed tournaments show on side panel only up to 30 days after finish date

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -6,6 +6,12 @@ class Tournament < ActiveRecord::Base
   has_many :referees
   has_many :users, :through => :referees
   
+  @referee_headroom = 30
+  
+  def self.referee_headroom
+    @referee_headroom
+  end 
+  
   def app_deadline_formatted
     if app_deadline
       return app_deadline.strftime("%d.%m.%Y, %H.%M")
@@ -15,7 +21,12 @@ class Tournament < ActiveRecord::Base
   def is_running?
     start_date <= Date.today and Date.today <= finish_date
   end
+  
+  # We give 30 days of headroom for referees to manage the aftergame
+  def is_relevant_for_referee?
+    Date.today <= finish_date + self.referee_headroom
+  end
     
   named_scope :registration_open, :conditions => ["app_deadline > ?", Time.now]
-  
+  named_scope :relevant_for_referee, :conditions => ["finish_date >= ?", Date.today - self.referee_headroom]
 end

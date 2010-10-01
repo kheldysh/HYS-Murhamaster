d3ffcee2fd207afc20b056a8eca12ef1300bbd92
kheldysh <kheldysh@ff25d568-61ca-42c6-9216-fe35c4661081>
2010-10-01 01:24:27 +0000

    changed back from referee_headroom to 30

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -6,12 +6,6 @@ class Tournament < ActiveRecord::Base
   has_many :referees
   has_many :users, :through => :referees
   
-  @referee_headroom = 30
-  
-  def self.referee_headroom
-    @referee_headroom
-  end 
-  
   def app_deadline_formatted
     if app_deadline
       return app_deadline.strftime("%d.%m.%Y, %H.%M")
@@ -24,9 +18,8 @@ class Tournament < ActiveRecord::Base
   
   # We give 30 days of headroom for referees to manage the aftergame
   def is_relevant_for_referee?
-    Date.today <= finish_date + self.referee_headroom
+    Date.today <= finish_date + 30
   end
     
   named_scope :registration_open, :conditions => ["app_deadline > ?", Time.now]
-  named_scope :relevant_for_referee, :conditions => ["finish_date >= ?", Date.today - self.referee_headroom]
 end

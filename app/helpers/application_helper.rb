c772f7e98a4c124c4fdab84e8759fd086b53a558
kheldysh <kheldysh@ff25d568-61ca-42c6-9216-fe35c4661081>
2011-04-25 22:39:12 +0000

    added helper function for delivering finnish dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include UserAuthentication
+
+  def finnish_date(date)
+    return date.strftime("%d.%m.%Y klo %H.%M")
+  end
   
 end

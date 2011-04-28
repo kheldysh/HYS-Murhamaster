1fb6958d7cfa7d2164e92dea02c8e8e307d14f8a
kheldysh <kheldysh@ff25d568-61ca-42c6-9216-fe35c4661081>
2011-04-28 21:31:02 +0000

    Assignments are now destroyed on cascade

--- app/models/ring.rb
+++ app/models/ring.rb
@@ -1,5 +1,5 @@
 class Ring < ActiveRecord::Base
-  has_many :assignments
+  has_many :assignments, :dependent => :destroy
   belongs_to :tournament
   accepts_nested_attributes_for :assignments, :allow_destroy => true
 end

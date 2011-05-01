95a6bc7d13b677ae218dcd331633ae6f5ca6cc71
kheldysh <kheldysh@ff25d568-61ca-42c6-9216-fe35c4661081>
2011-05-01 22:01:06 +0000

    added param checking, modified create and new for better usability in views

--- app/controllers/warrants_controller.rb
+++ app/controllers/warrants_controller.rb
@@ -12,15 +12,22 @@ class WarrantsController < ApplicationController
     @tournament = Tournament.find(params[:tournament_id])
     @active_players = @tournament.players.find_all{ |player| player.active? }
     @detectives = @tournament.players.find_all{ |player| player.detective? }
+    for detective in @detectives
+      @warrant.assignments.build(attributes = {:player => detective})
+    end
   end
 
   def create
     new_params = purge_assignments(params)
-
-    @tournament = Tournament.find(new_params[:tournament_id])
-    @warrant = Warrant.new(new_params[:warrant])
-    @warrant.tournament = @tournament
-    @warrant.save!
+    # if no assignments, don't create warrant
+    unless params[:warrant][:assignments_attributes].empty?
+      @target = Player.find(params[:warrant][:assignments_attributes]["0"][:target_id])
+      @tournament = Tournament.find(new_params[:tournament_id])
+      @warrant = Warrant.new(new_params[:warrant])
+      @warrant.target = @target
+      @warrant.tournament = @tournament
+      @warrant.save!
+    end
     redirect_to :tournament_warrants
   end
 
@@ -65,6 +72,31 @@ class WarrantsController < ApplicationController
     end
     redirect_to root_path
     return false
+  end
+
+  def purge_assignments(old_params)
+    logger.info("Purging parameters from faulty assignments")
+    if old_params[:warrant]
+      logger.info("Found multiple assignments")
+      # TODO: move this check to Assignment model and handle it properly
+      old_params[:warrant][:assignments_attributes].each do |key, ass|
+        # purge assignments without players or targets
+        if ["0", ""].include? ass[:player_id]  or ["0", ""].include? ass[:target_id]
+          logger.info("purging empty assignment: #{ass[:player_id]}->#{ass[:target_id]}")
+          old_params[:warrant][:assignments_attributes].delete(key)
+        # purge assignments to own self
+        elsif ass[:player_id] == ass[:target_id]
+          logger.info("purging self-pointing assignment: #{ass[:player_id]}->#{ass[:target_id]}")
+          old_params[:warrant][:assignments_attributes].delete(key)
+        end
+      end
+    elsif old_params[:assignment]
+      if old_params[:assignment][:player_id] == "0" or old_params[:assignment][:target_id] == "0"
+        logger.info("purging empty old_params[:assignment]ignment: #{old_params[:assignment][:player_id]}->#{old_params[:assignment][:target_id]}")
+        old_params.delete(:assignment)
+      end
+    end
+    return old_params
   end
 
 end

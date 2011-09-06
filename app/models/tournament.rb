9625053ddf42c1f17c1630eda73d780458899938
Jesse Lankila <kheldysh@kapsi.fi>
2011-09-06 22:06:35 +0300

    added tournament.is_over?

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -8,19 +8,23 @@ class Tournament < ActiveRecord::Base
   has_many :referees
   has_many :users, :through => :referees
   has_many :events
-  
+
   before_create :update_stats
-  
+
   def app_deadline_formatted
     if app_deadline
       return app_deadline.strftime("%d.%m.%Y, %H.%M")
     end
   end
-  
+
   def is_running?
     start_date <= Date.today and Date.today <= finish_date
   end
-  
+
+  def is_over?
+    finish_date < Date.today
+  end
+
   # We give 30 days of headroom for referees to manage the aftergame
   def is_relevant_for_referee?
     Date.today <= finish_date + 30
@@ -44,9 +48,10 @@ class Tournament < ActiveRecord::Base
       tournament.arrested += event.arrests
       tournament.collaterals += event.collaterals
       tournament.witnesses += event.witnesses
-      tournament.eyewitnesses += event.eyewitnesses      
+      tournament.eyewitnesses += event.eyewitnesses
     end
     tournament.save!
-  end  
+  end
 
 end
+

5cabbcf30d18d7d76767013b8c658b20fd21da28
Jesse Lankila <kheldysh@kapsi.fi>
2011-09-09 04:31:06 +0300

    target updating for referees

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,6 +1,7 @@
 class TargetsController < ApplicationController
 
-  before_filter :is_own_target_or_admin?
+  before_filter :is_own_target_or_referee?
+  before_filter :is_referee?, :only => :edit
 
   def show
     @target = Player.find(params[:id])
@@ -11,12 +12,46 @@ class TargetsController < ApplicationController
     @current_player = current_user.players.find(:first, :conditions => [ "tournament_id = ?", @tournament ])
 
   end
-  
-  def is_own_target_or_admin?
+
+  def edit
+    @target = Player.find(params[:id])
+    @user = @target.user
+    @tournament = @target.tournament
+    @calendar = @user.calendar
+  end
+
+
+  def update
+    @tournament = Tournament.find(params[:tournament_id])
+    @target = Player.find(params[:id])
+    @user = @target.user
+    @user.update_attributes(params[:user])
+    @calendar = @user.calendar
+    @calendar.update_attributes(params[:calendar])
+
+
+    if params[:picture] and params[:picture][:uploaded_picture]
+      @picture = Picture.new(params[:picture])
+      @picture.user = @user
+      @picture.save!
+    end
+
+    redirect_to tournament_target_path(@tournament, @target)
+
+  end
+
+
+  def is_own_target_or_referee?
     if current_user.admin
       return true
     end
-    
+
+    @tournament.referees.each do |referee|
+      if current_user.referees.include? referee
+        return true
+      end
+    end
+
     @target = Player.find(params[:id])
     current_user.players.each do |player|
       if player.targets.include? @target
@@ -27,3 +62,4 @@ class TargetsController < ApplicationController
     return false
   end
 end
+

98dbe73f697aa24f0d7f9d0899d3156ed82d74fd
Jesse Lankila <kheldysh@kapsi.fi>
2011-09-10 06:00:41 +0300

    added "waiting_approval" to possible player statuses

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,22 +4,28 @@ class Player < ActiveRecord::Base
   belongs_to :tournament
   belongs_to :team
   belongs_to :referee
-  
+
   has_one :warrant
   has_many :assignments
   has_many :targets, :through => :assignments
 
-  validates_inclusion_of :status, :in => [:active, :detective, :dead]
- 
+  validates_inclusion_of :status, :in => [:waiting_approval, :active, :detective, :dead]
+
+  named_scope :waiting_players, :conditions => ["status = ?", "waiting_approval"]
+
   named_scope :active_players, :conditions => ["status = ?", "active"]
   named_scope :dead_players, :conditions => ["status = ?", "dead"]
   named_scope :detectives, :conditions => ["status = ?", "detective"]
-   
+
   def status
     read_attribute(:status).to_sym
   end
   def status= (value)
     write_attribute(:status, value.to_s)
+  end
+
+  def waiting_approval?
+    self.status == :waiting_approval
   end
 
   def detective?
@@ -33,7 +39,7 @@ class Player < ActiveRecord::Base
   def dead?
     self.status == :dead
   end
-  
+
 
   def with_real_name
     "#{self.user.full_name}, #{self.alias}"
@@ -48,7 +54,8 @@ class Player < ActiveRecord::Base
       "%s (%s)" % [ self.alias, self.team.name ]
     else
       self.alias
-    end      
-  end    
+    end
+  end
 
 end
+

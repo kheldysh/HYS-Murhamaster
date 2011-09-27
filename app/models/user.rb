1b71ce8a72b53d48c3e1202f0dc1bace1c574a15
Jesse Lankila <kheldysh@kapsi.fi>
2011-09-27 23:19:15 +0300

    removed redundant logger lines that caused failed logins to show error page

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,6 @@ class User < ActiveRecord::Base
   def self.authenticate(username, password)
     user = User.find_by_username(username)
 
-    logger.info "found user %s" % user
-    logger.info "%s == %s" % [hash_plaintext_password(password), user.password]
     if user && user.password == hash_plaintext_password(password)
       logger.info "correct info"
       return user

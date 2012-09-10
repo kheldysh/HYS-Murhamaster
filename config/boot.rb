a9b17e5c511c34b7d55403c110e301676e3aef1a
Jesse Lankila <jesse.lankila@relex.fi>
2012-09-10 04:27:32 +0300

    update to rails 2.3.8

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,6 @@
 # Don't change this file!
 # Configure your app in config/environment.rb and config/environments/*.rb
 
-require 'thread' # added to make db:migrate work on prod env's rails 2.3.12
-
 RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
 
 module Rails
@@ -46,6 +44,7 @@ module Rails
     def load_initializer
       require "#{RAILS_ROOT}/vendor/rails/railties/lib/initializer"
       Rails::Initializer.run(:install_gem_spec_stubs)
+      Rails::GemDependency.add_frozen_gem_path
     end
   end
 
@@ -83,8 +82,8 @@ module Rails
       end
 
       def load_rubygems
+        min_version = '1.3.2'
         require 'rubygems'
-        min_version = '1.3.1'
         unless rubygems_version >= min_version
           $stderr.puts %Q(Rails requires RubyGems >= #{min_version} (you have #{rubygems_version}). Please `gem update --system` and try again.)
           exit 1
@@ -109,4 +108,3 @@ end
 
 # All that for this:
 Rails.boot!
-

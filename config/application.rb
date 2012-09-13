5fcab8b809be1a2f84c42825661712a2e193232b
Jesse Lankila <jesse.lankila@relex.fi>
2012-09-14 01:59:02 +0300

    use environment-declared secret token

--- config/application.rb
+++ config/application.rb
@@ -41,12 +41,9 @@ module HYSMurhamaster
                                  :university, :faculty, :department, :phone, :username, :other_notes,
                                  :name, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
 
-    config.autoload_paths += [config.root.join('lib')]
+    config.secret_token = ENV['SECRET_TOKEN']
 
-    #config..session.store = {
-    #  :session_key => '_murhamaster_session',
-    #  :secret      => '2c621468ec40b7d321ed7912a73de4dee4712c59084e2257a9a30f67d709a3a1511e60ce6c65a5b133e1871e17e9c72f5f078d3cad624a2f955a52a7d12a7c4e'
-    #}
+    config.autoload_paths += [config.root.join('lib')]
 
     config.time_zone = 'UTC'
 

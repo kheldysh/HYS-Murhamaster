4cb6d55f5e28d5d232462c756616e49b201239d0
Jesse Lankila <jesse.lankila@relex.fi>
2012-09-26 00:39:21 +0300

    revive code from old application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
-  include UserAuthentication # /lib/user_authentication.rb
+  include UserAuthentication
+
+  def finnish_date(date)
+    return date.strftime("%d.%m.%Y klo %H.%M")
+  end
+
+  def nil_date_handler(date)
+    if date == nil
+      return "Ei tietoa"
+    else
+      return finnish_date(date)
+    end
+  end
 end

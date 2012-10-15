f3d39d086accdbdf378271d576d928073ae71b06
Jesse Lankila <jesse.lankila@relex.fi>
2012-10-15 22:38:03 +0300

    add login helper method for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -36,3 +35,11 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 end
+
+def login(user)
+  def log_user_in(user)
+    session[:user_id] = user.id if user
+    session[:admin] = user.admin if user
+  end
+end
+

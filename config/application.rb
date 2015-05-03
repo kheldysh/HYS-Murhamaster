0ff7c61ba5982d370ba7cfc1dcd44592812a7165
Jesse Lankila <jesse.lankila@relex.fi>
2015-05-03 15:31:25 +0300

    Make ActiveSupport use local time instead of UTC as default

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,8 @@ module HYSMurhamaster
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Helsinki'
-
+    config.active_record.default_timezone = :local
+    
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
@@ -44,8 +45,6 @@ module HYSMurhamaster
     config.secret_token = ENV['SECRET_TOKEN']
 
     config.autoload_paths += [config.root.join('lib')]
-
-    config.time_zone = 'Helsinki'
 
   end
 end

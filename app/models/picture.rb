c65ad0fea1b616846301274d413e8a5f5b561ecc
Jesse Lankila <jesse.lankila@relex.fi>
2015-05-16 03:19:53 +0300

    Use paperclip to display quarter-size photo

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,18 +5,18 @@ class Picture < ActiveRecord::Base
   # TODO: add validations
 
   has_attached_file :photo,
-                    :styles => { :standard => '960x960>' },
-                    :storage => :s3,
-                    :s3_permissions => 'authenticated-read',
-                    :s3_credentials => {
-                        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                    styles: { standard: '960x960>', quarter: '480x480>' },
+                    storage: :s3,
+                    s3_permissions: 'authenticated-read',
+                    s3_credentials: {
+                        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
                     }, # File.join(Rails.root, 'config', 's3.yml'),
-                    :url => ':s3_domain_url',
-                    :path => '/assets/:class/:id/:style.:extension',
-                    :s3_server_side_encryption => :aes256,
-                    :s3_host_name => 's3-eu-west-1.amazonaws.com',
-                    :bucket => ENV['AWS_BUCKET']
+                    url: ':s3_domain_url',
+                    path: '/assets/:class/:id/:style.:extension',
+                    s3_server_side_encryption: :aes256,
+                    s3_host_name: 's3-eu-west-1.amazonaws.com',
+                    bucket: ENV['AWS_BUCKET']
 
   def url(style = :standard)
     Paperclip::Interpolations.interpolate('/:class/:id/:style.:extension', photo, style)
